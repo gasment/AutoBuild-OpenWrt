db58ed24ed9cb451aadac58094b81f1ea4e9e2fe
gasment <61599044+gasment@users.noreply.github.com>
2021-03-20 19:41:28 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.32.1/g' openwrt/package/base-files/files/bin/config_generate
 #version
-sed -i 's/Lede Build by Gas /@ $(TZ=UTC-8 date "+%Y.%m.%d") /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/Lede Build by Gas /@ $(TZ=UTC-8 date "+%Y.%m.%d") /g" openwrt/package/lean/default-settings/files/zzz-default-settings
